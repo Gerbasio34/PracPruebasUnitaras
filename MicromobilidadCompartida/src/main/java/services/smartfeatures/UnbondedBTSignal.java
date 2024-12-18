94b4047c20405fd8f7efc01267c0ba9f6fd1ab53
Gerbasio34 <torresbarrancogerman3@gmail.com>
2024-12-18 21:36:52 +0100

    Update UnbondedBTSignal.java
    FIX: Connection Exception

--- MicromobilidadCompartida/src/main/java/services/smartfeatures/UnbondedBTSignal.java
+++ MicromobilidadCompartida/src/main/java/services/smartfeatures/UnbondedBTSignal.java
@@ -1,7 +1,10 @@
 package services.smartfeatures;
+
+import java.net.ConnectException;
+
 /**
  * External services involved in the functioning of some features
  */
 public interface UnbondedBTSignal { // Broadcasts the station ID by BT
-    void BTbroadcast () throws ConnectException5;
+    void BTbroadcast () throws ConnectException;
 }
