dc0ac5ae9a880ab704ed9cb34c77c8cbca4f0963
Rapite46 <60982902+SotoHalf@users.noreply.github.com>
2024-12-18 21:22:53 +0100

    FIX: Duplicated Arduino

--- MicromobilidadCompartida/src/main/java/services/smartfeatures/ArduinoMicroController.java
+++ MicromobilidadCompartida/src/main/java/services/smartfeatures/ArduinoMicroController.java
@@ -1,4 +1,23 @@
+
 package services.smartfeatures;
 
+import java.net.ConnectException;
+
+/**
+ * External services involved in the functioning of some features
+ */
 public interface ArduinoMicroController {
+    // Software para microcontroladores
+
+    public void setBTconnection() throws ConnectException;
+
+    public void startDriving() throws PMVPhisicalException,
+            ConnectException,
+            ProceduralException;
+
+    public void stopDriving() throws PMVPhisicalException,
+            ConnectException,
+            ProceduralException;
+
+    public void undoBTconnection();
 }
