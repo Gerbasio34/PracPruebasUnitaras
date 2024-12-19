d855bd078291144a8dd12554ce1c7d8c7b3d2cd5
Rapite46 <60982902+SotoHalf@users.noreply.github.com>
2024-12-19 19:08:54 +0100

    ADD: Micromobility Jorney Realize Handler

---
+++ MicromobilidadCompartida/src/main/java/micromobility/JourneyRealizeHandler.java
@@ -0,0 +1,62 @@
+package micromobility;
+
+import data.GeographicPoint;
+import data.StationID;
+import exception.*;
+
+import java.net.ConnectException;
+import java.time.LocalDateTime;
+
+// Class that handles events and operations related to journey realization.
+public class JourneyRealizeHandler {
+
+    // Class members
+
+    // Constructors
+    public JourneyRealizeHandler() {
+        // Object initialization
+    }
+
+    // User interface input events
+    public void scanQR()
+            throws ConnectException, InvalidPairingArgsException,
+            CorruptedImgException, PMVNotAvailException,
+            ProceduralException {
+        // Implementation
+    }
+
+    public void unPairVehicle()
+            throws ConnectException, InvalidPairingArgsException,
+            PairingNotFoundException, ProceduralException {
+        // Implementation
+    }
+
+    // Input events from the unbonded Bluetooth channel
+    public void broadcastStationID(StationID stID)
+            throws ConnectException {
+        // Implementation
+    }
+
+    // Input events from the Arduino microcontroller channel
+    public void startDriving()
+            throws ConnectException, ProceduralException {
+        // Implementation
+    }
+
+    public void stopDriving()
+            throws ConnectException, ProceduralException {
+        // Implementation
+    }
+
+    // Internal operations
+    private void calculateValues(GeographicPoint gP, LocalDateTime date) {
+        // Implementation
+    }
+
+    private void calculateImport(float dis, int dur, float avSp, LocalDateTime date) {
+        // Implementation
+    }
+
+    // Setter methods for injecting dependences
+
+}
